f7beb593ffee01ff90a6bf0d2f4eba1ac4508f1f
i-william17 <williamsisulu2003@gmail.com>
2025-02-06 05:31:22 +0300

    Second

---
+++ constants/constants.tsx
@@ -0,0 +1,39 @@
+export const onboardingSwiperData: onboardingSwiperDataType[] = [
+    {
+      id: 1,
+      title: "Start your learning journey with dose of fun!",
+      description: "Our engaging learning materials will spark",
+      sortDescrition: "your curiosity & make learning enjoyable.",
+      image: require("@/assets/images/image2.png"),
+    },
+    {
+      id: 2,
+      title: "Discover your passion, elevate your expertise",
+      description: "Our comprehensive courses and expert",
+      sortDescrition: "instructors to guide you every step of the way.",
+      image: require("@/assets/images/image3.png"),
+    },
+    {
+      id: 3,
+      title: "Get online certificate with Unusual skills",
+      description: "To earn an online certificate, individuals",
+      sortDescrition: "typically need to complete the required",
+      sortDescrition2: "coursework & assessments.",
+      image: require("@/assets/images/image4.png"),
+    },
+  ];
+  
+  export const bannerData: BannerDataTypes[] = [
+    {
+      bannerImageUrl: require("@/assets/images/image2.png"),
+    },
+    {
+      bannerImageUrl: require("@/assets/images/image2.png"),
+    },
+    {
+      bannerImageUrl: require("@/assets/images/image2.png"),
+    },
+    {
+      bannerImageUrl: require("@/assets/images/image2.png"),
+    },
+  ];
