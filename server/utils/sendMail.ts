e2d7f145e9c1652abb80475d4b52effa18f3a2b0
i-william17 <williamsisulu2003@gmail.com>
2025-04-01 18:22:29 +0300

    Admin

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -13,8 +13,9 @@ interface EmailOptions{
 const sendMail = async (options: EmailOptions):Promise <void> => {
     const transporter: Transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '587'),
+        port: Number(process.env.SMTP_PORT || '587'),
         service: process.env.SMTP_SERVICE,
+        secure: process.env.SMTP_SECURE === '465',
         auth:{
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD,
